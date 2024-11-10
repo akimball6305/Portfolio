d8424d9629c63a6fc26debc7752b89415801af45
akimball6305 <aiden.kimball6305@gmail.com>
2024-11-10 17:53:49 -0600

    fixed things
    inverted images and added descriptions

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ function toggleMenu() {
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+
+/*const toggleButton = document.getElementById("mode-toggle");
+const body = document.body;
+
+// Initial mode
+let isDarkMode = true;
+
+toggleButton.addEventListener("click", () => {
+  // Toggle the light or dark mode class on the body
+  body.classList.toggle("light-mode", !isDarkMode);
+  body.classList.toggle("dark-mode", isDarkMode);
+
+  // Update the button text
+  toggleButton.textContent = isDarkMode ? "Switch to Dark Mode" : "Switch to Light Mode";
+
+  // Toggle the mode state
+  isDarkMode = !isDarkMode;
+}); */
